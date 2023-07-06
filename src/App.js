871f3b33f2fdd96cd4710122a7be4e7a3dba684b
LouisZhong <destinyxll@gmail.com>
2023-07-06 19:45:59 +0800

    feat: calendar date could be selected

--- src/App.js
+++ src/App.js
@@ -1,75 +1,9 @@
 import './Style.css'
-import moment from 'moment'
+import Calendar from 'components/Calendar.jsx';
 
-console.log(moment().format('DD MMM'))
-
-let dd = moment().format('DD MMMM YYYY')
-console.log(`this is ${dd}`)
 function App() {
   return (
-    <div className='container'>
-      <div className='input-container'>        
-        <p className="input-header">
-          <span className="material-symbols-outlined">event</span>
-          入住日期 - 退房日期
-        </p>
-        <p>time: {dd}</p>
-        
-        <input placeholder='2023/06/19 - 2023/06/20'/>
-      </div>
-      <div className="date-container">
-        <div className="date-header">
-          <button>Prev</button>
-          <button>Jun 2023</button>
-          <button>Next</button>
-        </div>
-        <div className="date-body">
-          <ul className="week-list">
-            <li><span>Sun</span></li>
-            <li><span>Mon</span></li>
-            <li><span>Tue</span></li>
-            <li><span>Wen</span></li>
-            <li><span>Thu</span></li>
-            <li><span>Fri</span></li>
-            <li><span>Sat</span></li>
-          </ul>
-          <ul className="date-list">
-            <li><span>1</span></li>
-            <li><span>2</span></li>
-            <li><span>3</span></li>
-            <li><span>4</span></li>
-            <li><span>5</span></li>
-            <li><span>6</span></li>
-            <li><span>7</span></li>
-            <li><span>8</span></li>
-            <li><span>9</span></li>
-            <li><span>10</span></li>
-            <li><span>11</span></li>
-            <li><span>12</span></li>
-            <li><span>13</span></li>
-            <li><span>14</span></li>
-            <li><span>15</span></li>
-            <li><span>16</span></li>
-            <li><span>17</span></li>
-            <li><span>18</span></li>
-            <li><span>19</span></li>
-            <li><span>20</span></li>
-            <li><span>21</span></li>
-            <li><span>22</span></li>
-            <li><span>23</span></li>
-            <li><span>24</span></li>
-            <li><span>25</span></li>
-            <li><span>26</span></li>
-            <li><span>27</span></li>
-            <li><span>28</span></li>
-            <li><span>29</span></li>
-            <li><span>30</span></li>
-            <li><span>31</span></li>
-          </ul>
-        </div>        
-      </div>
-    </div>
-    
+    <Calendar />
   );
 }
 
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,80 @@
+import moment from 'moment'
+import { useState } from 'react';
+
+
+
+
+export default function Calendar () {
+  const [currentMonth, setCurrentMonth] = useState(moment().format('MMM YYYY'))
+  const [currentDate,setCurrentDate] = useState(moment().format('YYYY/MM/DD'))
+  const currentYearAndMonth = moment().format('YYYY/MM')
+  console.log(currentYearAndMonth)
+  // const currentDate = moment().format('YYYY/MM/DD')
+  const daysInMonth = moment(currentMonth).daysInMonth()
+
+  // 取得當前月份的第一天是星期幾
+  const firstDayOfMonth = moment(currentMonth).startOf('momth').format('d')
+
+  // 用於填充第一天之前的空格
+  const blanks = []
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    blanks.push(<li><span></span></li>)
+  }
+
+  const days = []
+  for (let day = 1; day <= daysInMonth; day++) {
+    days.push(<li><span id={day}>{day}</span></li>)
+  }
+
+  function gotoNextMonth() {
+    const nextMonth = moment(currentMonth).add(1, 'month').format('MMM YYYY')
+    setCurrentMonth(nextMonth)
+  }
+
+  function gotoPrevtMonth() {
+    const prevtMonth = moment(currentMonth).subtract(1, 'month').format('MMM YYYY')
+    setCurrentMonth(prevtMonth)
+  }  
+
+  function handleDateSelect (event) {
+    console.log(event.target.id)
+    const datePicked = event.target.id
+    const selectedDate = `${currentYearAndMonth}/${datePicked}`
+    setCurrentDate(selectedDate)
+  }
+
+  return (
+    
+    <div className='container'>
+      <div className='input-container'>        
+        <p className="input-header">
+          <span className="material-symbols-outlined">event</span>
+          入住日期 - 退房日期
+        </p>
+        <input value={currentDate}/>
+      </div>
+      <div className="date-container">
+        <div className="date-header">
+          <button onClick={gotoPrevtMonth}>Prev</button>
+          <button>{currentMonth}</button>
+          <button onClick={gotoNextMonth}>Next</button>
+        </div>
+        <div className="date-body">
+          <ul className="week-list">
+            <li><span>Sun</span></li>
+            <li><span>Mon</span></li>
+            <li><span>Tue</span></li>
+            <li><span>Wen</span></li>
+            <li><span>Thu</span></li>
+            <li><span>Fri</span></li>
+            <li><span>Sat</span></li>
+          </ul>
+          <ul className="date-list"  onClick={handleDateSelect}>
+            {blanks}{days}
+          </ul>
+        </div>        
+      </div>
+    </div>
+  );
+}
+
